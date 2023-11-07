db963597fd3eb741272ef57d4b2c41ea8f500d61
adonijah01 <adonijahsingoei@gmail.com>
2023-11-07 18:03:21 +0300

    Count it!

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-0-subs
+subs
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    # Define the URL for the subreddit's about.json endpoint
+    # Defines the URL for the subreddit's about.json endpoint
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
-    # Set a custom User-Agent to avoid "Too Many Requests" errors
+    # Set's a custom User-Agent to avoid "Too Many Requests" errors
     headers = {'User-Agent': 'My Reddit API Client'}
 
     try:
-        # Make the HTTP GET request
+        # HTTP GET request
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for HTTP errors
 
-        # Parse the JSON response
+        
         data = response.json()
 
         # Extract the number of subscribers from the JSON data
@@ -34,7 +34,7 @@ def number_of_subscribers(subreddit):
 
 
 if __name__ == '__main__':
-    # You can add command-line argument handling here if needed
-    subreddit = "programming"  # Replace with the subreddit you want to check
+    
+    subreddit = "cybersecurity"  # Replace with the subreddit you want to check
     subscribers = number_of_subscribers(subreddit)
     print(subscribers)
